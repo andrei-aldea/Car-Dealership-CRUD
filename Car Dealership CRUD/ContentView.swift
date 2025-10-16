faa704f9404137b7a195e9a0b3e64ca87e2b2b12
Andrei Aldea <andreialdea2004@gmail.com>
2025-10-16 09:59:40 +0300

    added the basic struct and a view to display the cars

--- Car Dealership CRUD/ContentView.swift
+++ Car Dealership CRUD/ContentView.swift
@@ -7,16 +7,37 @@
 
 import SwiftUI
 
+struct Car: Identifiable {
+    let id = UUID()
+    var manufacturer: String
+    var model: String
+    var horsepower: Int
+    var price: Double
+}
+
 struct ContentView: View {
+    @State private var cars: [Car] = [
+            Car(manufacturer: "Ferrari", model: "F8 Tributo", horsepower: 710, price: 276550),
+            Car(manufacturer: "Lamborghini", model: "Huracan EVO", horsepower: 631, price: 261274),
+            Car(manufacturer: "Porsche", model: "911 GT3", horsepower: 502, price: 161100)
+        ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                List(cars) { car in
+                    VStack(alignment: .leading) {
+                        Text("\(car.manufacturer) \(car.model)")
+                            .font(.headline)
+                        Text("Horsepower: \(car.horsepower) hp")
+                            .font(.subheadline)
+                        Text("Price: $\(car.price, specifier: "%.2f")")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .navigationTitle("Car Inventory")
+            }
         }
-        .padding()
-    }
 }
 
 #Preview {
