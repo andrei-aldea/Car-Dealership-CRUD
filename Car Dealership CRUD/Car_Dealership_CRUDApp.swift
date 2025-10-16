d8ab5462e2fa99665c3ac352057d1acc84d01a81
Andrei Aldea <andreialdea2004@gmail.com>
2025-10-16 09:42:23 +0300

    Initial Commit

---
+++ Car Dealership CRUD/Car_Dealership_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_Dealership_CRUDApp.swift
+//  Car Dealership CRUD
+//
+//  Created by Andrei Aldea on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Car_Dealership_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
